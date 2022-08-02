e379b8e13cd3e20d208d080a36d6afd483750387
Trevor Sharp <sharp.trevor@me.com>
2022-08-02 03:24:18 +0000

    Improve word display

--- src/components/DisplayName.tsx
+++ src/components/DisplayName.tsx
@@ -2,16 +2,13 @@ type DisplayNameProps = {
   text: string;
 };
 
-const getShortestWordLength = (text: string) =>
-  text
-    .split(' ')
-    .sort((a, b) => a.length - b.length)
-    .find(() => true)?.length ?? 0;
+const hasMultipleShortWords = (text: string) =>
+  text.split(' ').filter((x) => x.length < 8).length > 1;
 
 const DisplayName = ({ text }: DisplayNameProps) => (
   <p
     className={`text-4xl font-bold break-word ${text.length < 20 ? 'w-fit' : 'w-min'} ${
-      text.length > 20 && getShortestWordLength(text) < 8 ? 'min-w-[16rem]' : ''
+      text.length > 20 && hasMultipleShortWords(text) ? 'min-w-[16rem]' : ''
     }`}
   >
     {text.slice(0, Math.min(text.length, 38)) + ((text.length ?? 0) > 38 ? '...' : '')}

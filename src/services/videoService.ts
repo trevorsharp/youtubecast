95ecb042d52ea7c4aab99f1e54567330cbcaeda2
Trevor Sharp <sharp.trevor@me.com>
2022-11-02 22:11:44 +0000

    Remove pytube dependency / Fully integrate ytdl-core

--- src/services/videoService.ts
+++ src/services/videoService.ts
@@ -1,7 +1,6 @@
-import { spawn } from 'child_process';
 import NodeCache from 'node-cache';
-import { Quality } from '../types';
 import ytdl from 'ytdl-core';
+import { Quality } from '../types';
 
 const cache = new NodeCache({ checkperiod: 120 });
 
@@ -31,34 +30,34 @@ const getStream = async (
 
   const videoUrl = await getVideoUrl(videoId, quality);
 
-  if (videoUrl === '' || !videoUrl) throw `Video not found with id ${videoId}`;
+  if (!videoUrl) throw `Video not found with id ${videoId}`;
 
   cache.set(cacheKey, videoUrl, 3600);
 
   return videoUrl;
 };
 
-const getVideoUrl = async (videoId: string, quality: Quality): Promise<string | undefined> =>
-  await spawnChild([`https://www.youtube.com/watch?v=${videoId}`, quality.toString()]);
+const getVideoUrl = async (videoId: string, quality: Quality): Promise<string | undefined> => {
+  const videoInfo = await ytdl.getInfo(`https://www.youtube.com/watch?v=${videoId}`);
 
-const spawnChild = async (args: string[]) => {
-  const [url, quality] = args;
-  const ytdlInfo = await ytdl.getInfo(url as string);
+  const formats = videoInfo.formats
+    .filter(
+      (format) =>
+        format.hasAudio &&
+        format.container === 'mp4' &&
+        (!format.hasVideo || format.videoCodec?.includes('avc'))
+    )
+    .sort((a, b) => (b.audioBitrate ?? 0) - (a.audioBitrate ?? 0))
+    .sort((a, b) => (b.height ?? 0) - (a.height ?? 0));
+
   switch (quality) {
-    case '0':
-      return ytdlInfo.formats
-        .filter((format) => format.qualityLabel === '720p')
-        .filter((format) => format.hasAudio)[0]?.url;
-    case '1':
-      return ytdlInfo.formats
-        .filter((format) => format.hasAudio)
-        .filter((format) => format.hasVideo === false)[0]?.url;
-    case '2':
-      return ytdlInfo.formats
-        .filter((format) => format.qualityLabel === '360p')
-        .filter((format) => format.hasAudio)[0]?.url;
+    case Quality.Audio:
+      return formats.find((format) => !format.hasVideo)?.url;
+    case Quality.P360:
+      return formats.find((format) => format.qualityLabel === '360p')?.url;
+    default:
+      return formats.find((format) => format.hasVideo)?.url;
   }
-  return '';
 };
 
 export { getStream };

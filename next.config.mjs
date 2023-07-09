d83eb4c95d06967dcb22cbf4845297bd6f3e9aba
Trevor Sharp <sharp.trevor@me.com>
2023-07-09 16:04:58 +0000

    Comment out i18n config

--- next.config.mjs
+++ next.config.mjs
@@ -30,10 +30,10 @@ const config = {
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en',
-  },
+  // i18n: {
+  //   locales: ['en'],
+  //   defaultLocale: 'en',
+  // },
 };
 
 export default config;

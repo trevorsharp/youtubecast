15f4ff32759e2b4d504f98dc0abc804a9b07011d
Trevor Sharp <sharp.trevor@me.com>
2024-07-20 17:20:58 +0000

    Fix prettier issue

--- src/app/[sourceId]/[[...additionalRoute]]/page.tsx
+++ src/app/[sourceId]/[[...additionalRoute]]/page.tsx
@@ -1,8 +1,10 @@
 import MainPage from '~/components/MainPage';
 import getSearchText from '~/utils/getSearchText';
 
-const Page = ({ params }: { params: { sourceId: string, additionalRoute?: string[] } }) => (
-  <MainPage searchText={getSearchText([params.sourceId, ...params.additionalRoute ?? []].join('/'))} />
+const Page = ({ params }: { params: { sourceId: string; additionalRoute?: string[] } }) => (
+  <MainPage
+    searchText={getSearchText([params.sourceId, ...(params.additionalRoute ?? [])].join('/'))}
+  />
 );
 
 export default Page;

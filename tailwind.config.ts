abdf864640f85612c3c535a599d96ca6d5f49f0e
Trevor Sharp <sharp.trevor@me.com>
2024-08-08 00:34:13 +0000

    Update ytdl-core

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
-import { type Config } from 'tailwindcss';
-import { fontFamily } from 'tailwindcss/defaultTheme';
+import type { Config } from 'tailwindcss';
 
 export default {
   content: ['./src/**/*.tsx'],

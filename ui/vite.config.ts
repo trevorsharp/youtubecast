0cd73ed0a34b75e1106815e142b8041b34de25f4
Trevor Sharp <sharp.trevor@me.com>
2024-12-20 04:36:39 +0000

    Cleanup config files

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,4 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default defineConfig({ plugins: [react()] });

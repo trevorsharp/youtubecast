701180f922c69bc57d150174ac8a2b91a5aeabfe
Trevor Sharp <sharp.trevor@me.com>
2024-12-24 16:38:19 +0000

    Try fixing audio drift

--- src/services/videoService.ts
+++ src/services/videoService.ts
@@ -69,7 +69,7 @@ const getCookies = async () => {
 };
 
 const getFfmpegOptions = () => ({
-  raw: '-hide_banner -c:v copy -c:a copy -f hls -hls_playlist_type vod -hls_flags single_file',
+  raw: '-hide_banner -r 30 -c:v copy -c:a aac -f hls -hls_playlist_type vod -hls_flags single_file',
 });
 
 export default { getStreamingVideoUrl, downloadVideo };

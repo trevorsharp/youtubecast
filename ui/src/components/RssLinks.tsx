bcedce71a2162274dda5f4b89eda24ca9982580d
Trevor Sharp <sharp.trevor@me.com>
2025-04-02 03:33:45 +0000

    Simplify audio only check box

--- ui/src/components/RssLinks.tsx
+++ ui/src/components/RssLinks.tsx
@@ -2,11 +2,11 @@ import { useState } from 'react';
 
 type RssLinksProps = {
   feedId: string;
-  audioOnly: boolean;
 };
 
-const RssLinks = ({ feedId, audioOnly }: RssLinksProps) => {
+const RssLinks = ({ feedId }: RssLinksProps) => {
   const [copiedText, setCopiedText] = useState<string>('');
+  const [audioOnly, setAudioOnly] = useState(false);
 
   const getRssLink = () => `${window.location.hostname}/${feedId}/feed${audioOnly ? '?audioOnly' : ''}`;
 
@@ -27,6 +27,18 @@ const RssLinks = ({ feedId, audioOnly }: RssLinksProps) => {
           <img className="h-10 w-10" src="/assets/pocketcasts.svg" alt="Pocket Casts" />
         </a>
         <img className="h-10 w-10 cursor-pointer" src="/assets/rss.svg" alt="RSS" onClick={copyRssLink} />
+      </div>
+      <div className="form-check flex gap-1">
+        <input
+          className="h-4 w-4 rounded-sm border border-gray-300 bg-white checked:border-youtube checked:bg-youtube focus:outline-none"
+          type="checkbox"
+          id="audio-only"
+          checked={audioOnly}
+          onChange={() => setAudioOnly((previousValue) => !previousValue)}
+        />
+        <label className="inline-block cursor-pointer select-none" htmlFor="audio-only">
+          Audio Only
+        </label>
       </div>
       {copiedText && <p>{copiedText}</p>}
     </div>
--- ui/src/components/SearchResult.tsx
+++ ui/src/components/SearchResult.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react';
 import { indexRoute } from '../routes';
-import AudioOnlySelection from './AudioOnlySelection';
 import RssLinks from './RssLinks';
 
 const SearchResult = () => {
   const feedData = indexRoute.useLoaderData();
-
-  const [audioOnly, setAudioOnly] = useState(false);
 
   if (!feedData) {
     return <p>Failed to search for channel or playlist</p>;
@@ -18,8 +14,7 @@ const SearchResult = () => {
         <img className="h-16 w-16 rounded-full" src={feedData.imageUrl} alt="Profile" />
         <p className="max-w-md text-4xl font-bold whitespace-nowrap truncate">{feedData.name}</p>
       </a>
-      <AudioOnlySelection audioOnly={audioOnly} setAudioOnly={setAudioOnly} />
-      <RssLinks feedId={feedData.id} audioOnly={audioOnly} />
+      <RssLinks feedId={feedData.id} />
     </div>
   );
 };

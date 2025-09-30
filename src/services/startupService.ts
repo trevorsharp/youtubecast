64586bb7b809a782013fbcd54d5e15de0d84bf03
Trevor Sharp <sharp.trevor@me.com>
2025-09-30 22:40:37 +0000

    Fix startup cleanup content folder

--- src/services/startupService.ts
+++ src/services/startupService.ts
@@ -5,8 +5,8 @@ import env from '../env';
 const startApplication = async () => {
   await configService.getConfig();
   await $`yt-dlp -U`;
-  await $`find ${env.CONFIG_FOLDER_PATH} -name "*.video" -type f -delete`;
-  await $`find ${env.CONFIG_FOLDER_PATH} -name "*.audio" -type f -delete`;
+  await $`find ${env.CONTENT_FOLDER_PATH} -name "*.video" -type f -delete`;
+  await $`find ${env.CONTENT_FOLDER_PATH} -name "*.audio" -type f -delete`;
 };
 
 export default { startApplication };
